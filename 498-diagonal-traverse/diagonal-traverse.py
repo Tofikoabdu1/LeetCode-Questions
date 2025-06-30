2d5652193f7b0ea28e636b09dc1b24383554a04e
Tofik Abdulkerim <165275773+Tofikoabdu1@users.noreply.github.com>
2025-06-30 11:38:08 +0300

    Time: 11 ms (65.83%) | Memory: 20.8 MB (27.23%) - LeetSync

---
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        num_rows=len(mat)
+        num_cols=len(mat[0])
+        
+        diagonals=[[] for _ in range(num_rows + num_cols - 1)]
+
+        for i in range(num_rows):
+            for j in range(num_cols):
+                diagonals[i+j].append(mat[i][j])
+        res=diagonals[0]
+        for x in range(1,len(diagonals)):
+            if x%2==1:
+                res.extend(diagonals[x])
+            else:
+                diagonals[x].reverse()
+                res.extend(diagonals[x])
+        return res
+        
