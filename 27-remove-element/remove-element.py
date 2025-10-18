898698320b0517447cb7e606e3ed346456ddbd0b
Tofik Abdulkerim <165275773+Tofikoabdu1@users.noreply.github.com>
2025-10-18 10:25:17 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (11.18%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -2,13 +2,13 @@ class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         seek=0
         hold=0
-        occur=0
+        occur=nums.count(val)
         while seek < len(nums):
             if nums[seek]!=val:
                 nums[seek],nums[hold]=nums[hold],nums[seek]
                 hold+=1
-            elif nums[seek]==val:
-                occur+=1
+            # elif nums[seek]==val:
+            #     occur+=1
             seek+=1
         return len(nums)-occur
 
