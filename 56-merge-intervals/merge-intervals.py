ee4d3636018bcaa239c9fbfdf66a8b6703271248
Tofik Abdulkerim <165275773+Tofikoabdu1@users.noreply.github.com>
2026-02-04 10:45:47 +0300

    Time: 10 ms (32.83%) | Memory: 22.5 MB (37.20%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -9,7 +9,7 @@ class Solution:
         while j < len(intervals):
             if i == 0:
                 if intervals[i][1] >= intervals[j][0] and intervals[i][1] >= intervals[j][1] :
-                    res.append([intervals[i][0],intervals[i][1]])
+                    res.append(intervals[i])
                     i+=1
                 elif intervals[i][1] >= intervals[j][0]:
                      res.append([intervals[i][0],intervals[j][1]])
