5011467cbf686155f89bac81daa51eae05fef995
Tofik Abdulkerim <165275773+Tofikoabdu1@users.noreply.github.com>
2026-02-05 15:44:48 +0300

    Time: 11 ms (53.78%) | Memory: 19.8 MB (34.73%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -1,20 +1,14 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        s1=set(list1)
-        s2=set(list2)
-        s3= s1 & s2
-        l3 = list(s3)
-        if len(l3) == 1:
-            return l3
-        idx = 20000
-        x =[]
-        for i in l3:
-            idx = min(idx , (list1.index(i) + list2.index(i)))
-        print(l3)
-        print(idx)
-        for i in l3:
-            y = list1.index(i) + list2.index(i)
-            if y == idx:
-                x.append(i)
-        return x
-        
+        d1 = {name : idx for idx, name in enumerate(list1)}
+        res = []
+        min_idx = 2000
+        for i , val in enumerate(list2):
+            if val in d1:
+                y = i + d1[val]
+                if y < min_idx:
+                    min_idx = y
+                    res=[val]
+                elif y == min_idx:
+                    res.append(val)
+        return res
