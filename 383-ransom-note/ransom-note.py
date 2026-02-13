296e0f68604c1b88f313773c7d161382b30c670b
Tofik Abdulkerim <165275773+Tofikoabdu1@users.noreply.github.com>
2026-02-13 14:24:53 +0300

    Time: 12 ms (84.59%) | Memory: 19.6 MB (49.69%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        c1 =Counter(ransomNote)
+        c2 =Counter(magazine)
+        for i in c1:
+            if i not in c2 or c1[i]>c2[i]:
+                return False
+        return True
+        
