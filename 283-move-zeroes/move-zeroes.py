0153160d875ce40e4d6b3690164943cd06a79925
Tofik Abdulkerim <165275773+Tofikoabdu1@users.noreply.github.com>
2026-02-23 18:02:38 +0300

    Time: 3 ms (82.75%) | Memory: 20.6 MB (21.02%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,13 +4,7 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         hold = 0
-        # seek = 0
-        # while seek < len(nums):
-        #     if nums[seek] != 0:
-        #         nums[seek],nums[hold] = nums[hold],nums[seek]
-        #         hold+=1
-        #     seek+=1
         for seek in range(len(nums)):
             if nums[seek]!=0:
-                        nums[seek],nums[hold] = nums[hold],nums[seek]
-                        hold+=1
+                nums[seek],nums[hold] = nums[hold],nums[seek]
+                hold+=1
