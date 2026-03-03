d0768a9769a38d041b14ddcc905361f6a2f4f7bc
Tofik Abdulkerim <165275773+Tofikoabdu1@users.noreply.github.com>
2026-03-03 17:35:07 +0300

    Time: 69 ms (29.02%) | Memory: 43.3 MB (20.24%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -1,5 +1,9 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        d = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g',
+             7: 'h', 8: 'i', 9: 'j', 10: 'k', 11: 'l', 12: 'm', 13: 'n', 
+             14: 'o', 15: 'p', 16: 'q', 17: 'r', 18: 's', 19: 't', 20: 'u', 
+             21: 'v', 22: 'w', 23: 'x', 24: 'y', 25: 'z'}
         l=[0]*(len(s)+1)
         for i in shifts:
             if i[2]==0:
@@ -11,10 +15,15 @@ class Solution:
         pref=[l[0]]
         for i in range(1,len(s)):
             pref.append(pref[-1]+l[i])
-        # print(pref)
-        res=[]
-        for i in range(len(s)):
-            newc =( ord(s[i]) - ord('a') + pref[i] ) % 26 + ord('a')
-            res.append(chr(newc))
-        return (''.join(res))
+        chars = [ord(i)-ord('a') for i in s]
+
+        for i in range(len(chars)):
+            x =chars[i]+pref[i]
+          
+            chars[i] = x%26
+        print(chars)
+        let = [d[i] for i in chars]
+        return ''.join(let)
+   
+       
 
