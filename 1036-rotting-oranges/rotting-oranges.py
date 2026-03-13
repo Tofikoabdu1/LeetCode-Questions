2a31cb07c43a7802ca6078b6005adf63834cffb3
Tofik Abdulkerim <165275773+Tofikoabdu1@users.noreply.github.com>
2026-03-14 00:04:23 +0300

    Time: 3 ms (76.08%) | Memory: 19.3 MB (71.81%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -5,6 +5,7 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
         q=deque()
+        drxn = [[-1, 0], [0, -1], [1, 0], [0, 1]]
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 2:
@@ -17,22 +18,13 @@ class Solution:
             for i in range(l):
                 r ,c = q.popleft()
                 # print(r , c)
-                if r+1 < rows and grid[r+1][c]==1:
-                    grid[r+1][c] = 2
-                    q.append([r+1,c])
-                    fresh-=1
-                if r-1 >= 0 and grid[r-1][c]==1:
-                    grid[r-1][c] = 2
-                    q.append([r-1,c])
-                    fresh-=1
-                if c+1 < cols and grid[r][c+1]==1:
-                    grid[r][c+1]=2
-                    q.append([r,c+1])
-                    fresh-=1
-                if c-1 >=0 and grid[r][c-1]==1:
-                    grid[r][c-1]=2
-                    q.append([r,c-1])
-                    fresh-=1
+                for dx, dy in drxn:
+                    nr = r + dx
+                    nc = c + dy
+                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                        grid[nr][nc]= 2
+                        fresh -= 1
+                        q.append([nr, nc])
         if fresh > 0:
             return -1
         return minutes
