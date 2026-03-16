25dc87db3fd5f1f852ed036da67dc5651620537d
Tofik Abdulkerim <165275773+Tofikoabdu1@users.noreply.github.com>
2026-03-16 17:30:37 +0300

    Time: 1136 ms (5.03%) | Memory: 19.1 MB (83.42%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,6 +1,17 @@
 class Solution:
+    memo = [-1]*31
     def fib(self, n: int) -> int:
-        if n == 0 or n == 1:
-            return n
-        return self.fib(n-1) + self.fib(n-2)
+        def sol(n):
+            memo = [-1]*31
+            if n == 0 or n == 1:
+                return n
+            first = -1
+            second = -1
+            if memo[n-1] != -1: return memo[n-1]
+            else: first = sol(n-1)
+            if memo[n-2]!= -1: return momo[n-2]
+            else: second = sol(n-2)
+            memo[n]= first+second
+            return memo[n]
+        return sol(n)
     
