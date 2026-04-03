0c15b659d9fd89d6f2ed4b7d8bbf6d58a0eb8fac
Tofik Abdulkerim <165275773+Tofikoabdu1@users.noreply.github.com>
2026-04-03 22:06:11 +0300

    Time: 3 ms (75.89%) | Memory: 20.4 MB (65.35%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        hash = defaultdict(int)
+        graph = defaultdict(list)
+        for i , j in prerequisites:
+            hash[i]+=1
+            graph[j].append(i)
+        # print(hash)
+        for i in range(numCourses):
+            if i not in hash:
+                hash[i] = 0
+        # print(hash , graph)
+        q = deque()
+        res = []
+        for i , j in hash.items():
+            if j == 0:
+                q.append(i)
+        # print(q)
+        while q:
+            curr = q.popleft()
+            res.append(curr)
+            for i in graph[curr]:
+                hash[i]-=1
+                if hash[i]==0:
+                    q.append(i)
+        # print(hash)
+        if len(res) == numCourses:
+            return res
+        return []
+
+            
+            
+
+        
