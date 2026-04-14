c5517ecaa312a466f5a4cae29e5d21c648f14d3b
Tofik Abdulkerim <165275773+Tofikoabdu1@users.noreply.github.com>
2026-04-14 11:00:22 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (20.55%) - LeetSync

---
+++ 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minMoves(self, target: int, maxDoubles: int) -> int:
+        cnt = 0
+        
+        while target > 1 and maxDoubles > 0:
+            if target % 2 == 0:
+                target //= 2
+                maxDoubles -= 1
+            else:
+                target -= 1
+            cnt += 1
+
+        cnt += target - 1
+        
+        return cnt
+            
